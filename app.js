228aaff1ae341b6baa841a49a735f5e5e655b776
George Redinger <george@georgeredinger.com>
2013-12-05 14:59:59 -0800

    Sorta works, redirect accel.py to file, tail the file into node app.js Not yet graphing

--- app.js
+++ app.js
@@ -1,77 +1,60 @@
+#!/usr/bin/env node
+
 var app = require('http').createServer(handler)
-    , io = require('socket.io').listen(app)
-    , fs = require('fs')
-    , SerialPort = require("serialport").SerialPort
-    , serialPort = new SerialPort("/dev/ttyACM0", {
-baudrate: 115200
-    });
+, io = require('socket.io').listen(app)
+, fs = require('fs');
 
-    var start = new Buffer(['B',0xFF, 0x08, 0x07, 0x00, 0x00, 0x00, 0x00]);
-    var request = new Buffer(['B',0xFF, 0x07, 0x03]);
-    var result = new Buffer(8);
+process.stdin.setEncoding('utf8');
 
-    function handler (req, res) {
-        fs.readFile(__dirname + '/index.html',
-                    function (err, data) {
-                        if (err) {
-                            res.writeHead(500);
-                            return res.end('Error loading index.html');
-                        }
+process.stdin.resume();
 
-                        res.writeHead(200);
-                        res.end(data);
-                    });
-    }
+ process.stdout.on('error', function epipeFilter(err) {
+ 	if (err.code === 'EPIPE') return process.exit();
+ 		// If there's more than one error handler (ie, us), then the error won't be bubbled up anyway
+ 		if (process.stdout.listeners('error').length <= 1) {
+ 			process.stdout.removeAllListeners(); // Pretend we were never here
+ 			process.stdout.emit('error', err); // Then emit as if we were never here
+ 			process.stdout.on('error', epipeFilter); // Then reattach, ready for the next error!
+ 		}
+ });
 
-    io.sockets.on('connection', function (socket) {
-        server.on('message', function (message, remote) {
-            console.log( message);
-            var accel =  {x:0,y:0};
-            accel = JSON.parse(message);
-            console.log(accel);
-            socket.emit("accel",accel);
-        });
 
-    });
+app.listen(3000);
 
-    serialPort.on('open',function() {
-        console.log('Port open');
-        serialPort.write(start,function(err,results) {
-            console.log("starting |" + err + "|" + results);
-            serialPort.write(start,function(err,results){
-                console.log("first request |"+err+"|"+results);
-            });
-        }); 
+//	process.stdin.on('data', function(data) {
+//		console.log("rraw:"+data);
+////		var accel=  {x:getRan(),y:getRan(),z:getRan()};
+//		var accel = JSON.parse(data);
+//		console.log("ccooked:"+accel);
+//	});
 
+function handler (req, res) {
+	fs.readFile(__dirname + '/index.html',
+							function (err, data) {
+								if (err) {
+									res.writeHead(500);
+									return res.end('Error loading index.html');
+								}
+
+								res.writeHead(200);
+								res.end(data);
+							});
+}
+
+io.sockets.on('connection', function (socket) {
+	console.log("got socket connection");
+	process.stdin.on('data', function(data) {
+		console.log("raw:"+data);
+		var accel =  {x:0,y:0,z:0};
+		accel = JSON.parse(data);
+		console.log("cooked:"+accel);
+		socket.emit("accel",accel);
+	});
 });
 
-serialPort.on('data', function(data) {
-  console.log(data);
-  serialPort.write(request,function(err,results){
-        console.log("next request |"+err+"|"+results);
-     });
-});
 
 
-//   def startAccessPoint():$
-//         return array.array('B', [0xFF, 0x07, 0x03]).tostring()$
-//    
-//    def accDataRequest():$
-//         return array.array('B', [0xFF, 0x08, 0x07, 0x00, 0x00, 0x00, 0x00]).tostring()$
-//         21 $
-//           23 $
-//            24  $
-//             25 #Start access point$
-//              26 ser.write(startAccessPoint())$
-//               27  $
-//                28  $
-//                 29 while True:$
-//                      30     #Send request for acceleration data$
-//                           31     ser.write(accDataRequest())$
-//                    32     accel = ser.read(7)$
-//                     33     if(len(accel)>3):$
-//                          34        if ord(accel[0]) != 0 and ord(accel[1]) != 0 and ord(accel[2]) != 0:$
-//                               35          print "x: " + str(ord(accel[0])) + " y: " + str(ord(accel[1])) + " z: " + str(ord(accel[2]))$
-//                        36    $
-//                        37    $
-//
+ function getRan() {
+   return Math.random() * (255);
+   }
+
